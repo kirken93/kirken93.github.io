9632aea0d6e03907c8dc2b8ce6bae4cc51db7c71
Megan <kirken93@comcast.net>
2015-08-24 21:08:21 -0400

    Improved age script

--- script.js
+++ script.js
@@ -2,9 +2,13 @@ function myAge() {
   var birthday = new Date(1993,7,9).getTime();
   var age = document.getElementById("age");
 
-  var current_date = new Date().getTime();
-  var seconds_since = (current_date - birthday) / 1000;
+  var current_date = new Date();
+  var seconds_since = (current_date.getTime() - birthday) / 1000;
 
   var years = (seconds_since / 31536000).toFixed(5);
+  if (years < (current_date.getFullYear() - 1993))
+    if (current_date.getMonth() >= 6 && current_date.getDate() >=9)
+      years = current_date.getFullYear() - 1993;
+
   age.innerHTML = years;
 }
