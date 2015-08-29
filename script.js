588660a53e6b551861d95329f7585cac2b89cf7b
Megan <kirken93@comcast.net>
2015-08-28 21:32:01 -0400

    Fixed shift cipher

--- script.js
+++ script.js
@@ -12,3 +12,50 @@ function myAge() {
 
   age.innerHTML = years;
 }
+
+function shiftCipher(encrypt, text, key) {
+   output = "";
+    for (i=0; i<text.length; i++){
+      newNum = (ascii(text.charCodeAt(i)) + (encrypt ? key : (26-key))) % 26;
+      output = output.concat(String.fromCharCode(ascii(newNum)));
+    }
+    return output;
+}
+
+function affineCipher(encrypt, text, a, b){
+  output = "";
+  for (i=0; i<text.length; i++){
+    output += (ascii(text.charAt(i)) + a*ascii(text.charAt(i))+b % 26 + 26) % 26;
+    //output += (ascii(text.charAt(i)) + (encrypt ? a*ascii(text.charAt(i))+b : inverse(a)*ascii(text.charAt(i))-b)) % 26;
+  //  output += (ascii(text.charAt(i)) + (encrypt ? a*ascii(text.charAt(i))+b : inverse(a)*(ascii(text.charAt(i) - b)) % 26 + 26 ) % 26));
+  }
+  document.getElementById('affine').innerHTML = output;
+}
+
+function ascii(letter){
+  if (letter < 26) //return ascii value
+      return (letter + 65);
+  else if (letter > 64 && letter < 91) //return capital letter value
+      return (letter - 65);
+  else if (letter > 96 && letter < 123) //return capital letter value
+      return (letter - 97);
+  else
+      return -1;
+}
+
+
+function inverse(num){
+  if (value == 1) return 1;
+  else if (value == 3) return 9;
+	else if (value == 5) return 21;
+	else if (value == 7) return 15;
+	else if (value == 9) return 3;
+	else if (value == 11) return 19;
+	else if (value == 15) return 7;
+	else if (value == 17) return 23;
+	else if (value == 19) return 11;
+	else if (value == 21) return 5;
+	else if (value == 23) return 17;
+	else if (value == 25) return 25;
+	else return 0;
+}
